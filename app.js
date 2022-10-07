1a3e9d70bb39a0a38249fb7b527d8b15c34d3f40
david-duartes <dduartes@gmail.com>
2022-10-07 12:31:17 -0300

    Add curse code

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const path = require('path');
 
 const express = require('express');
-
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+  })
+);
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -16,7 +24,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
-
 app.use(shopRoutes);
 
 app.use((req, res, next) => {

0b107edf58769380404307c4315234c3fe9a11c5
david-duartes <dduartes@gmail.com>
2023-01-25 21:23:37 -0300

    Add security routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -9,14 +11,65 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false,
+  });
+};
+
 exports.postLogin = (req, res, next) => {
-  User.findById('63670219ddeb8a399fd34c39')
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email })
     .then((user) => {
-      req.session.user = user;
-      req.session.isAuthenticated = true;
-      req.session.save(() => {
-        res.redirect('/');
-      });
+      if (!user) {
+        return res.redirect('/login');
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then((isEqual) => {
+          if (isEqual) {
+            req.session.user = user;
+            req.session.isAuthenticated = true;
+            return req.session.save(() => {
+              res.redirect('/');
+            });
+          }
+          res.redirect('/login');
+        })
+        .catch((err) => {
+          console.log(err);
+          res.redirect('/login');
+        });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        return res.redirect('/');
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const newUser = new User({
+            email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+
+          return newUser.save();
+        })
+        .then(() => res.redirect('/login'));
     })
     .catch((err) => console.log(err));
 };
--- models/user.js
+++ models/user.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  name: { type: String, required: true }, // String is shorthand for {type: String}
   email: { type: String, required: true },
+  password: { type: String, required: true },
   cart: {
     items: [
       {
